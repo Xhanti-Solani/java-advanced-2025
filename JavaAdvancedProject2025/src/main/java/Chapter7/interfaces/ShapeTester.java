11a9e1582e836ead2d55d14dcf9d64cb840f4be8
xhanti-solani <xhanti.solani@ilabqa.com>
2025-09-23 15:18:12 +0200

    update main

---
+++ JavaAdvancedProject2025/src/main/java/Chapter7/interfaces/ShapeTester.java
@@ -0,0 +1,12 @@
+package Chapter7.interfaces;
+
+public class ShapeTester {
+    public static void main(String[] args) {
+
+        MyRectangle rectangle = new MyRectangle(6, 9);
+
+        rectangle.display();
+        rectangle.draw();
+
+    }
+}
